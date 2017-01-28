96f77f839d734633f36728898511863c8295e0f7
Carolyn Lam <caro_lyn_@hotmail.com>
2017-01-28 17:47:50 -0500

    change the root of the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+
+  # controller name then # before route name
+  root 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
